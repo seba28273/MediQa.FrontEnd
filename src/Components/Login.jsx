73dd0fc6c11fa0a84caac765c82f1cb1b649aff1
Sebastian Bustos <sebastian28273@hotmail.com>
2022-11-14 14:15:37 -0300

    V20221114 Seba 1

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import { Form, Field } from 'react-final-form';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
@@ -6,7 +6,9 @@ import { Password } from 'primereact/password';
 import { Dialog } from 'primereact/dialog';
 import { Divider } from 'primereact/divider';
 import { classNames } from 'primereact/utils';
+
 import '../css/App.css';
+
 
 export default function  Login() {
     const [showMessage, setShowMessage] = useState(false);
@@ -30,32 +32,37 @@ export default function  Login() {
        
     const [state, setstate] = useState();
     const [login, setlogin] = useState([]);
-    const fetchlogin  = async (user, pass) => {
-        var myHeaders = new Headers();
-        myHeaders.withCredentials = true;
-        myHeaders.crossorigin = true;
-        myHeaders.method = "GET";
-        myHeaders.cache = "default";
-        
-        await fetch("http://200.115.185.55:4990/LoginAsync?user=" + user + "&password=" +  pass  , { myHeaders })
-        .then((response) =>  response.json())
-        .then((data) =>  setlogin(data.data))
-        .catch((error) => console.log(error));
 
-    }
-    const onSubmit = (data, form) => {
-        setFormData(data);
-        setShowMessage(true);
-        fetchlogin(data.user, data.password);
+
+const fetchlogin  =  (user, pass) => {
+    var myHeaders = new Headers();
+    myHeaders.withCredentials = true;
+    myHeaders.crossorigin = true;
+    myHeaders.method = "GET";
+    myHeaders.cache = "default";
+    
+     fetch("http://200.115.185.55:4990/LoginAsync?user=" + user + "&password=" +  pass  , { myHeaders })
+    .then((response) =>  response.json())
+    .then((data) =>  setlogin(data.data))
+    .catch((error) => console.log(error));
+
+}
+   
+const onSubmit = (data, form) => {
+
+    setFormData(data);
+    fetchlogin(data.user, data.password);
+    setShowMessage(true);
+    
         
-       
-        console.log(login); 
-         
-        alert(login.nombreApellido);   
-        alert(login.eMail);    
-        //this.props.history.push('/Usuarios');
-        //form.restart();
-    };
+    
+    
+    console.log(login); 
+        
+    alert(login.nombreApellido);   
+    alert(login.eMail);  
+    
+};
 
     const isFormFieldValid = (meta) => !!(meta.touched && meta.error);
     const getFormErrorMessage = (meta) => {
@@ -78,47 +85,49 @@ export default function  Login() {
     );
 
     return (
-        <div className="form-register">
-            <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
-                <div className="flex align-items-center flex-column pt-6 px-3">
-                    <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
-                    <h5>Ingreso exitoso!</h5>
-                    
-                </div>
-            </Dialog>
-
-            <div className="flex justify-content-center">
-                <div className="card">
-                    <h5 className="text-center">Login</h5>
-                    <Form onSubmit={onSubmit} initialValues={{user: '', password: '', accept: false }} validate={validate} render={({ handleSubmit }) => (
-                        <form onSubmit={handleSubmit} className="p-fluid">
+    
+                <div className="form-register">
+                    <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
+                        <div className="flex align-items-center flex-column pt-6 px-3">
+                            <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
+                            <h5>Ingreso exitoso!</h5>
                             
-                            <Field name="user" render={({ input, meta }) => (
-                                <div className="field">
-                                    <span className="p-float-label">
-                                        <InputText id="user" {...input} 
-                                         className={classNames({ 'p-invalid': isFormFieldValid(meta) })} />
-                                        <label htmlFor="user" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Usuario*</label>
-                                    </span>
-                                    {getFormErrorMessage(meta)}
-                                </div>
+                        </div>
+                    </Dialog>
+
+                    <div className="flex justify-content-center">
+                        <div className="card">
+                            <h5 className="text-center">Login</h5>
+                            <Form onSubmit={onSubmit} initialValues={{user: '', password: '', accept: false }} validate={validate} render={({ handleSubmit }) => (
+                                <form onSubmit={handleSubmit} className="p-fluid">
+                                    
+                                    <Field name="user" render={({ input, meta }) => (
+                                        <div className="field">
+                                            <span className="p-float-label">
+                                                <InputText id="user" {...input} 
+                                                className={classNames({ 'p-invalid': isFormFieldValid(meta) })} />
+                                                <label htmlFor="user" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Usuario*</label>
+                                            </span>
+                                            {getFormErrorMessage(meta)}
+                                        </div>
+                                    )} />
+                                
+                                    <Field name="password" render={({ input, meta }) => (
+                                        <div className="field">
+                                            <span className="p-float-label">
+                                                <Password id="password" {...input} toggleMask className={classNames({ 'p-invalid': isFormFieldValid(meta) })} header={passwordHeader} footer={passwordFooter} />
+                                                <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Password*</label>
+                                            </span>
+                                            {getFormErrorMessage(meta)}
+                                        </div>
+                                    )} />
+                                
+                                    <Button type="submit" label="Submit" className="mt-2" />
+                                </form>
                             )} />
-                           
-                            <Field name="password" render={({ input, meta }) => (
-                                <div className="field">
-                                    <span className="p-float-label">
-                                        <Password id="password" {...input} toggleMask className={classNames({ 'p-invalid': isFormFieldValid(meta) })} header={passwordHeader} footer={passwordFooter} />
-                                        <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Password*</label>
-                                    </span>
-                                    {getFormErrorMessage(meta)}
-                                </div>
-                            )} />
-                           
-                            <Button type="submit" label="Submit" className="mt-2" />
-                        </form>
-                    )} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+       
     );
 }
